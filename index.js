2b8e7a9a28326555b6143bc302abaabfcb6d49e1
Marta Rubio <martarubiodiaz@hotmail.com>
2024-09-04 17:14:26 +0100

    alias and fragment

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ const schemaSDL = fs.readFileSync(
 // build the schema
 const schema = buildSchema(schemaSDL);
 
+const dataContent = [
+  { id: 1, name: "Content 1", copy: "Copy 1", description: "Description 1" },
+  { id: 2, name: "Content 2", copy: "Copy 2", description: "Description 2" },
+];
+
+const products = [
+  { id: 1, type: "t-shirt", price: "9,50", colour: "red", size: "M" },
+  { id: 2, type: "hoodie", price: "45", colour: "gray", size: "L" },
+  { id: 3, type: "cap", price: "5,50", colour: "blue", size: null },
+  { id: 4, type: "t-shirt", price: "18", colour: "yellow", size: "S" },
+];
+
 const root = {
   me: () => {
     return {
@@ -29,36 +41,25 @@ const root = {
       total: 100,
     };
   },
-  dataContent: () => {
-    return [
-      {
-        id: 1,
-        name: "John",
-        copy: "Hello World",
-        description: "This is a description",
-      },
-      {
-        id: 2,
-        name: "Doe",
-        copy: "Goodbye World",
-        description: "This is a description",
-      },
-    ];
+  dataContent: (args) => {
+    if (!args.id && !args.name) {
+      return dataContent;
+    }
+
+    return dataContent.filter(
+      (content) =>
+        (!args.id || content.id === args.id) &&
+        (!args.name || content.name === args.name)
+    );
   },
   products: (args) => {
-    const products = [
-      { id: 1, type: "t-shirt", price: "9,50", colour: "red", size: "M" },
-      { id: 2, type: "hoodie", price: "45", colour: "gray", size: "L" },
-      { id: 3, type: "cap", price: "5,50", colour: "blue", size: null },
-      { id: 4, type: "t-shirt", price: "18", colour: "yellow", size: "S" },
-    ];
     // if no arguments are passed, return all products
     if (!args.id && !args.type) {
       return products;
     }
     // if arguments are passed, return the product with the id and type that we passed as arguments
-    return products.find(
-      (product) => product.id === args.id && product.type === args.type
+    return products.filter(
+      (product) => (!args.id || product.id === args.id) && (!args.type || product.type === args.type)
     );
   },
 };
