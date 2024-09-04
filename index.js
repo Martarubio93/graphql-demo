bc56eb042e3b42751807531e4cf7979fc6145cab
Marta Rubio <martarubiodiaz@hotmail.com>
2024-09-04 12:26:49 +0100

    graphql schema

--- index.js
+++ index.js
@@ -1,97 +1,67 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLInt,
-  GraphQLList,
-} = require("graphql");
+const { buildSchema } = require("graphql");
+const fs = require("fs");
+const path = require("path");
 
-const cardType = new GraphQLObjectType({
-  name: "Cart",
-  fields: {
-    id: { type: GraphQLInt },
-    quantity: { type: GraphQLInt },
-    total: { type: GraphQLInt },
+// load the SDL schema from the file
+const schemaSDL = fs.readFileSync(
+  path.join(__dirname, "schema.graphql"),
+  "utf8"
+);
+
+// build the schema
+const schema = buildSchema(schemaSDL);
+
+const root = {
+  me: () => {
+    return {
+      id: 1,
+      name: "Marta",
+      email: "marta@gmail.com",
+    };
   },
-});
 
-const dataType = new GraphQLObjectType({
-  name: "DataContent",
-  fields: {
-    id: { type: GraphQLInt },
-    name: { type: GraphQLString },
-    copy: { type: GraphQLString },
-    description: { type: GraphQLString },
+  cart: () => {
+    return {
+      id: 1,
+      quantity: 2,
+      total: 100,
+    };
   },
-});
-
-const productType = new GraphQLObjectType({
-    name: "Product",
-    fields:{
-        id: {type: GraphQLInt},
-        type: {type: GraphQLString},
-        price: {type : GraphQLString},
-        colour: {type: GraphQLString},
-        size: {type: GraphQLString},
+  dataContent: () => {
+    return [
+      {
+        id: 1,
+        name: "John",
+        copy: "Hello World",
+        description: "This is a description",
+      },
+      {
+        id: 2,
+        name: "Doe",
+        copy: "Goodbye World",
+        description: "This is a description",
+      },
+    ];
+  },
+  products: (args) => {
+    const products = [
+      { id: 1, type: "t-shirt", price: "9,50", colour: "red", size: "M" },
+      { id: 2, type: "hoodie", price: "45", colour: "gray", size: "L" },
+      { id: 3, type: "cap", price: "5,50", colour: "blue", size: null },
+      { id: 4, type: "t-shirt", price: "18", colour: "yellow", size: "S" },
+    ];
+    // if no arguments are passed, return all products
+    if (!args.id && !args.type) {
+      return products;
     }
-});
-
-const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
-  fields: {
-    cart: {
-      type: cardType,
-      resolve() {
-        const cartData = {
-          id: 1,
-          quantity: 2,
-          total: 100,
-        };
-        return cartData;
-      },
-    },
-    dataContent: {
-      type: new GraphQLList(dataType),
-      resolve() {
-        const dataList = [
-          {
-            id: 1,
-            name: "John",
-            copy: "Hello World",
-            description: "This is a description",
-          },
-          {
-            id: 2,
-            name: "Doe",
-            copy: "Goodbye World",
-            description: "This is a description",
-          },
-        ];
-        return dataList;
-      },
-    },
-
-    products: {
-      type: productType,
-      args: {id: {type : GraphQLInt}},
-      resolve(parent, args) {
-        const products = [
-          { id: 1, type: "t-shirt", price: "9,50", colour: "red", size: "M" },
-          { id: 2, type: "hoodie", price: "45", colour: "gray", size: "L" },
-          { id: 3, type: "cap", price: "5,50", colour: "blue", size: null },
-          { id: 4, type: "t-shirt", price: "18", colour: "yelow", size: "S" },
-        ];
-        return products.find((product) => product.id === args.id);
-      },
-    },
+    // if arguments are passed, return the product with the id and type that we passed as arguments
+    return products.find(
+      (product) => product.id === args.id && product.type === args.type
+    );
   },
-});
-
-const schema = new GraphQLSchema({
-  query: RootQuery,
-});
+};
 
 const app = express();
 
@@ -99,6 +69,7 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
+    rootValue: root,
     graphiql: true,
   })
 );
